a5a6052c18a3a346ec1a4aab363605f5c100e999
Mykolazaz <mykolas.motiejunas@gmail.com>
2024-12-27 17:31:13 +0200

    Added user choice for analyzing words or URLs in main function

--- main.cpp
+++ main.cpp
@@ -9,15 +9,26 @@ int main() {
     std::cout << "\nChoose a file to analyze: " << std::endl;
     std::cin >> fileName;
     
-
     FileAnalyzer analyzer("tlds-alpha-by-domain.txt");
 
     try {
-        analyzer.analyzeFile("vilnius-wiki.txt");
-        analyzer.outputRepeatedWords("repeated_words.txt");
-        analyzer.outputWordLocations("word_locations.txt");
-        analyzer.extractUrls("vilnius-wiki.txt");
-        analyzer.printUniqueUrls();
+        int choice;
+        std::cout << "Analyse words (0) or URLs (1)?" << std::endl;
+        std::cin >> choice;
+        if (choice == 0) {
+            analyzer.analyzeFile(fileName);
+            analyzer.outputRepeatedWords("repeated_words.txt");
+            analyzer.outputWordLocations("word_locations.txt");
+            std::cout << "Results saved to repeated_words.txt and word_locations.txt" << std::endl;
+        }
+        else if (choice == 1) {
+            analyzer.extractUrls(fileName);
+            analyzer.printUniqueUrls();
+        }
+        else {
+            std::cout << "Invalid choice" << std::endl;
+            EXIT_FAILURE;
+        }
     }
     catch (const std::exception& e) {
         std::cerr << "An error occurred: " << e.what() << std::endl;
